4a311eb30bda942ec1b76c21fe3b92e162db1e83
Getty708 <yesyes708@gmail.com>
2016-11-05 00:15:18 +0900

    [add] UserService

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,8 @@
-import { Component } from '@angular/core';
-
-import { User }      from './user';
+import { Component, OnInit } from '@angular/core';
 
 
-const USERS: User[]= [
-    { id: 'OSK13001', name: '13A', marks: 85 },
-    { id: 'OSK13002', name: '13B', marks: 75 },
-    { id: 'OSK13003', name: '13C', marks: 65 },
-    { id: 'OSK14001', name: '14A', marks: 55 },
-    { id: 'OSK14002', name: '14B', marks: 45 },
-    { id: 'OSK14003', name: '14C', marks: 35 },
-    { id: 'OSK15005', name: '15A', marks: 25 },
-    { id: 'OSK15006', name: '15B', marks: 15 },
-];
-
+import { User }        from './user';
+import { UserService } from './user.service';
 
 
 @Component({
@@ -21,12 +10,27 @@ const USERS: User[]= [
     selector: 'my-app',
     templateUrl: 'app.component.html',
     styleUrls: [ 'app.component.css' ],
+    providers: [ UserService ],
 })
 
 
-export class AppComponent {
-    
+export class AppComponent implements OnInit {
     title = 'Coach-X';
-    users = USERS;		// ユーザーデータの読み込み
+    users: User[];
+    selectedUser: User;
+    
+    constructor(
+	private userService: UserService,
+    ){}
+
+
+    getUsers(): void{
+	this.userService.getUsers()
+	    .then(users => this.users = users);	
+    }
+
+    ngOnInit(): void{
+	this.getUsers();
+    }
 
 }
