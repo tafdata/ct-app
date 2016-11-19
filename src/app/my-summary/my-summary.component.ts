b67c6773dcc56377e7b39edbb2630e65497abb9b
Getty708 <yesyes708@gmail.com>
2016-11-20 01:11:14 +0900

    [Add] my-summary

--- src/app/my-summary/my-summary.component.ts
+++ src/app/my-summary/my-summary.component.ts
@@ -10,13 +10,19 @@ import { User }                     from '../user';
 export class MySummaryComponent implements OnChanges {
 
     @Input() user: User;
-    
+    scoreTotal: number = 4567;
+    scoreMax: number = 10000;
+    goalAvg: number;
+    rankTotal: number = 15;
+    SP1: string = "三段跳";
+    SP2: string = "走幅跳";
+
 
     
   constructor() { }
 
     ngOnChanges() {
-	console.log(this.user);
+	this.goalAvg  = Math.floor((this.scoreTotal / this.scoreMax) * 1000) / 10;
     }
 
 }
