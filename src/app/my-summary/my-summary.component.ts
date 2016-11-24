4a1e7e5dc112115732cd3ffeea958c05c2e705a3
Getty708 <yesyes708@gmail.com>
2016-11-24 21:08:35 +0900

    [Modify] recordをInput()で受け取るように変更@my-summary

--- src/app/my-summary/my-summary.component.ts
+++ src/app/my-summary/my-summary.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, OnChanges } from '@angular/core';
 
 import { User }                     from '../user';
+import { Item }                     from '../item';
+import { ItemService }              from '../item.service';
+import { Mark }                     from '../mark';
+import { Record }                   from '../record';
+import { RecordService }            from '../record.service';
+
 
 @Component({
   selector: 'app-my-summary',
@@ -10,19 +16,31 @@ import { User }                     from '../user';
 export class MySummaryComponent implements OnChanges {
 
     @Input() user: User;
-    scoreTotal: number = 4567;
+    @Input() record: Record;
+    mark: Mark;
     scoreMax: number = 10000;
-    goalAvg: number;
-    rankTotal: number = 15;
-    SP1: string = "三段跳";
-    SP2: string = "走幅跳";
-
+    goalAvg: number = 0;
+    SP1: Item;
+    SP2: Item;
 
     
-  constructor() { }
+    constructor(
+	private itemService: ItemService,
+	private recordService: RecordService,
+    ) { }
 
-    ngOnChanges() {
-	this.goalAvg  = Math.floor((this.scoreTotal / this.scoreMax) * 1000) / 10;
+
+    ngOnChanges(changes: any) {
+	// 総合結果を取得
+	this.mark = this.record.records.find(mark => mark.id === 0);
+ 	this.goalAvg  = Math.floor((this.mark.score / this.scoreMax) * 1000) / 10;
+
+	if(this.user.SP1){
+	    this.SP1 = this.itemService.getItem(this.user.SP1);
+	}
+	if(this.user.SP2){
+	    this.SP2 = this.itemService.getItem(this.user.SP2);
+	}
     }
 
 }
